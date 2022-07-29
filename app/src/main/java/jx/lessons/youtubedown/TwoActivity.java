b46d0b20646d8a120d6e3bcdde4b970bdd4f4412
jamshid5818 <jamshidakkforgit@gmail.com>
2022-07-29 15:10:55 +0500

    You tube downloader

---
+++ app/src/main/java/jx/lessons/youtubedown/TwoActivity.java
@@ -0,0 +1,150 @@
+package jx.lessons.youtubedown;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import at.huber.youtubeExtractor.VideoMeta;
+import at.huber.youtubeExtractor.YouTubeExtractor;
+import at.huber.youtubeExtractor.YtFile;
+
+public class TwoActivity extends AppCompatActivity {
+
+    private static String youtubeLink;
+    private LinearLayout mainLayout;
+    private ProgressBar mainProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+        mainLayout = findViewById(R.id.main_layout);
+        mainProgressBar = findViewById(R.id.prgrBar);
+        permissions();
+        // Check how it was started and if we can get the youtube link
+        if (savedInstanceState == null && Intent.ACTION_SEND.equals(getIntent().getAction())
+                && getIntent().getType() != null && "text/plain".equals(getIntent().getType())) {
+
+            String ytLink = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+
+            if (ytLink != null
+                    && (ytLink.contains("://youtu.be/") || ytLink.contains("youtube.com/watch?v="))) {
+                youtubeLink = ytLink;
+                // We have a valid link
+                getYoutubeDownloadUrl(youtubeLink);
+            } else {
+                Toast.makeText(this, R.string.error_no_yt_link, Toast.LENGTH_LONG).show();
+                finish();
+            }
+        } else if (savedInstanceState != null && youtubeLink != null) {
+            getYoutubeDownloadUrl(youtubeLink);
+        } else {
+            finish();
+        }
+    }
+
+    private void getYoutubeDownloadUrl(String youtubeLink) {
+        new YouTubeExtractor(this) {
+
+            @Override
+            public void onExtractionComplete(SparseArray<YtFile> ytFiles, VideoMeta vMeta) {
+                mainProgressBar.setVisibility(View.GONE);
+
+                if (ytFiles == null) {
+                    // Something went wrong we got no urls. Always check this.
+                    finish();
+                    return;
+                }
+                // Iterate over itags
+                for (int i = 0, itag; i < ytFiles.size(); i++) {
+                    itag = ytFiles.keyAt(i);
+                    // ytFile represents one file with its url and meta data
+                    YtFile ytFile = ytFiles.get(itag);
+
+                    // Just add videos in a decent format => height -1 = audio
+                    if (ytFile.getFormat().getHeight() == -1 || ytFile.getFormat().getHeight() >= 360) {
+                        addButtonToMainLayout(vMeta.getTitle(), ytFile);
+                    }
+                }
+            }
+        }.extract(youtubeLink);
+    }
+
+    private void addButtonToMainLayout(final String videoTitle, final YtFile ytfile) {
+        // Display some buttons and let the user choose the format
+        String btnText = (ytfile.getFormat().getHeight() == -1) ? "Audio " +
+                ytfile.getFormat().getAudioBitrate() + " kbit/s" :
+                ytfile.getFormat().getHeight() + "p";
+        btnText += (ytfile.getFormat().isDashContainer()) ? " dash" : "";
+        Button btn = new Button(this);
+        btn.setText(btnText);
+        btn.setOnClickListener(v -> {
+            String filename;
+            if (videoTitle.length() > 55) {
+                filename = videoTitle.substring(0, 55) + "." + ytfile.getFormat().getExt();
+            } else {
+                filename = videoTitle + "." + ytfile.getFormat().getExt();
+            }
+            filename = filename.replaceAll("[\\\\><\"|*?%:#/]", "");
+            downloadFromUrl(ytfile.getUrl(), videoTitle, filename);
+            finish();
+        });
+        mainLayout.addView(btn);
+    }
+
+    private void downloadFromUrl(String youtubeDlUrl, String downloadTitle, String fileName) {
+        Uri uri = Uri.parse(youtubeDlUrl);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setTitle(downloadTitle);
+
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+    }
+    void permissions(){
+       if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+           if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+               ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
+
+           }
+       }
+       if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+           if (!Environment.isExternalStorageManager()){
+               try {
+                   Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                   intent.addCategory("android.intent.category.DEFAULT");
+                   intent.setData(Uri.parse(String.format("package:%s",this.getPackageName())));
+                   startActivityIfNeeded(intent,101);
+               }catch (Exception e){
+                   Log.d("EXCEPTION PERMISSION", e.getMessage());
+                   Intent intent = new Intent();
+                   intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                   startActivity(intent);
+               }
+           }
+       }
+    }
+
+}
